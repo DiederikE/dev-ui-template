fad8158fe5a2eaa96b968f0eebd272ac4beffefe
Diederik Eyckmans <diederikey@Diederiks-MacBook-Pro.local>
2022-11-12 21:52:16 +0100

    some changes

--- template-13/components/Work.tsx
+++ template-13/components/Work.tsx
@@ -3,7 +3,7 @@ import Card from "./Card";
 
 export default function Work() {
   const data = [
-    { flex: "flex-row-reverse", image: "/dlwsmartfactory.png", datasmall: "Azure, .NET, Java, React", datah1: "Mobile Web & IoT Project ", datap: "During my internship in delaware I had the opportunity to work on a project within the Mobile Web & IoT team. During this project I developed software around a miniature Industry 4.0 factory.", databtnhref: "https://www.delaware.pro/en-be/blogs/industry-4-0-discrete-manufacturing?utm_source=linkedin&utm_medium=socialseeder&utm_campaign=industry-40-discrete-manufacturing" },
+    { flex: "flex-row-reverse", image: "/smartfactoryposter.png", datasmall: "Azure, .NET, Java, React", datah1: "Mobile Web & IoT Project ", datap: "During my internship in delaware I had the opportunity to work on a project within the Mobile Web & IoT team. During this project I developed software around a miniature Industry 4.0 factory.", databtnhref: "https://www.delaware.pro/en-be/blogs/industry-4-0-discrete-manufacturing?utm_source=linkedin&utm_medium=socialseeder&utm_campaign=industry-40-discrete-manufacturing" },
     { flex: "", image: "/bandmanagerposter.png", datasmall: ".NET Blazor", datah1: "Bandmanager ", datap: "During my student days I worked for Codewolf for a while. One of the projects I have mainly worked on is Bandmanager.", databtnhref: "https://www.bandmanager.be/" },
   ];
   return (
